59b6ff3ea8e93e621b8ee45bb2410488e53cd898
Brandon <69767056+BrandonCarp@users.noreply.github.com>
2021-08-03 19:51:30 -0400

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,18 +1,14 @@
 
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
-let x =  300;
-let y = 180;
-let speedX = 0;
-let speedY = 0;
+let x =  10;
+let y = 10;
+let speedX = 1;
+let speedY = 10;
 let rimuruHeight = 15;
 let rimuruWidth = 15;
-let rimuruX = (canvas.width)/2;
-let rimuruY = (canvas.height)/2;
-
-
-
-
+let rimuruX = (canvas.width-rimuruWidth)/2;
+let rimuruY = (canvas.height-rimuruHeight)/2;
 
 let rightPressed = false;
 let leftPressed = false;
@@ -23,42 +19,66 @@ let downPressed = false;
 
 
 document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+
 
 
 function keyDownHandler(e) {
   if(e.key == "Right" || e.key == "ArrowRight") {
       rightPressed = true;
+      leftPressed = false;
+      upPressed = false;
+      downPressed = false;
   }
   else if(e.key == "Left" || e.key == "ArrowLeft") {
-      
-    leftPressed = true;
-  } 
-  else if(e.key == "Up" || e.key =="ArrowUp") {
-      upPressed = true;
-  } 
-  else if(e.key == "Down" || e.key =="ArrowDown") {
-    downPressed = true;
+    rightPressed = false;
+      leftPressed = true;
+      upPressed = false;
+      downPressed = false;
+  } else if(e.key == "Up" || e.key =="ArrowUp") {
+    rightPressed = false;
+    leftPressed = false;
+    upPressed = true;
+    downPressed = false;
+} else if(e.key == "Down" || e.key =="ArrowDown") {
+  rightPressed = false;
+  leftPressed = false;
+  upPressed = false;
+  downPressed = true;
 }
 }
 
-function keyUpHandler(e) {
-  if(e.key == "Right" || e.key == "ArrowRight") {
-      rightPressed = false;
-  }
-     else if(e.key == "Left" || e.key == "ArrowLeft") {
-      leftPressed = false;
+
+
+
+
+function gameOver(){
+  if(rimuruX + rimuruWidth > canvas.width || rimuruX <= 0) {
+   rimuruX = canvas.width/2;
+   rimuruY = canvas.height/2;
+
+   rightPressed = false;
+   leftPressed = false;
+   upPressed = false;
+   downPressed = false;
+
+  } else if (rimuruY >= canvas.height || rimuruY < 0) {
+     rimuruX = canvas.width/2;
+     rimuruY = canvas.height/2;
+
+     rightPressed = false;
+     leftPressed = false;
+     upPressed = false;
+     downPressed = false;
   } 
-  
-  else if(e.key == "Up" || e.key =="ArrowUp") {
-    upPressed = false;
-  }    
-  
-  else if(e.key ==  "Down" || e.key =="ArrowDown") {
-  downPressed = false;
-}
 }
 
+// function collisionDetection(){
+//   let rimuruYCenter = rimuruY + (rimuruHeight/2);
+//   if(rimuruYCenter < y-35 || rimuruYCenter > y+35) {
+//      x = 0;
+//      y = 0;
+//   }
+// }
 
 function drawRimuru(){
   ctx.beginPath();
@@ -68,11 +88,9 @@ function drawRimuru(){
     ctx.closePath();
 }
 
-
-
 function drawMinerals(){
   ctx.beginPath();
-  ctx.rect(40, 50, 15, 15);
+  ctx.rect(150, 90, 10, 10);
   ctx.fillStyle = "green";
   ctx.fill();
   ctx.closePath();
@@ -80,30 +98,30 @@ function drawMinerals(){
 
 function draw(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  drawRimuru();
   drawMinerals();
-
-
-  if(rightPressed && rimuruX < canvas.width-rimuruWidth) {
+  drawRimuru();
+  gameOver();
+  // collisionDetection();
+  
+  
+if(rightPressed && rimuruX < canvas.width-rimuruWidth) {
     rimuruX += 4;
 }
 else if(leftPressed && rimuruX > 0) {
     rimuruX -= 4;
 }
-
-if(upPressed && rimuruY < canvas.height-rimuruWidth) {
+else if(upPressed && rimuruY < canvas.height-rimuruHeight) {
   rimuruY -= 4;
 }
-else if(leftPressed && rimuruY > 0) {
+else if(downPressed && rimuruY > 0) {
   rimuruY += 4;
 }
 
 
-x += speedX;
-y += speedY;
+
+
+// x += speedX;
+// y += speedY;
 requestAnimationFrame(draw);
 }
-
-
 draw();
