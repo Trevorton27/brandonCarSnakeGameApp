693aa6170def6d924eadf318d2c41ab848297d07
Brandon <69767056+BrandonCarp@users.noreply.github.com>
2021-08-19 20:37:56 -0400

    Add files via upload

--- snake.js
+++ snake.js
@@ -1,17 +1,24 @@
-const cvs = document.getElementById('snake');
+const cvs = document.getElementById('gameCanvas');
 const ctx = cvs.getContext('2d');
-let slimeHeight = 15;
-let slimeWidth = 15;
-let slimeX = (cvs.width-slimeWidth)/2;
-let slimeY = (cvs.height-slimeHeight)/2;
+// Box Width
+const bw = cvs.width;
+// Box Height
+const bh = cvs.height;
+// Padding
+const p = 10;
+
+
+let keys = [];
+
 let rightPressed;
 let leftPressed;
 let upPressed;
 let downPressed;
 
+
 document.addEventListener('keydown', keyDownHandler, false);
 
-// Switch to key code eventually
+
 function keyDownHandler(e) {
   if(e.key == "Right" || e.key == "ArrowRight") {
       rightPressed = true;
@@ -35,132 +42,94 @@ function keyDownHandler(e) {
   upPressed = false;
   downPressed = true;
  }
+ e.preventDefault();
+}
+ 
+
+function getRandomWidth() {
+  return Math.floor(Math.random() * 500 / 25) * 25;
+}
+function getRandomHeight() {
+  return Math.floor(Math.random() * 400 / 25) * 25;
 }
 
-function gameOver(){
-  if(slime.x + slime.w > cvs.width || slime.x <= 0) {
-   slime.x = cvs.width/2;
-   slime.y = cvs.height/2;
-   rightPressed = false;
-   leftPressed = false;
-   upPressed = false;
-   downPressed = false;
 
-  } else if (slime.y >= cvs.height || slime.y < 0) {
-     slime.x = cvs.width/2;
-     slime.y = cvs.height/2;
-     rightPressed = false;
-     leftPressed = false;
-     upPressed = false;
-     downPressed = false;
-  } 
+
+let food = {
+  x : getRandomWidth(),
+  y : getRandomHeight(),
+  w : 24,
+  h : 24,
+  color : "red"
 }
+
+let slime = {
+  x : cvs.width / 2,
+  y : cvs.height / 2,
+  w : 24,
+  h : 24,
+  color : "blue",
+  speed : 4
+}
+
+
+let canvas = {
+  x : 0,
+  y : 0,
+  w : cvs.width,
+  h : cvs.height,
+  color : "green"
+};
 
 function drawRect(x,y,w,h,color){
   ctx.fillStyle = color;
   ctx.fillRect(x, y, w, h);
 }
 
-function drawCircle(x,y,r,color){
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.arc(x,y,r,0,Math.PI*2,false);
-  ctx.closePath();
-  ctx.fill();
-}
-
-function newFood(){
-  food = {
-   x : Math.floor(Math.random() * cvs.width),
-   y : Math.floor(Math.random() * cvs.height),
-   w : 13,
-   h : 13,
-   color : "brown"
- };
-}
-
-
-let newSlime;
-function slimeSize() {
-    newSlime = {
-      x : slimeX + 5,
-      y : slimeY + 5,
-      w : slimeWidth + 5,
-      h : slimeHeight + 5,
-      color : "blue"
-    }
-
-}
-
-function rectsColliding(){
-  if(slime.x > food.x + food.w ||
-    slime.x + slime.w < food.x ||
-    slime.y > food.y + food.h ||
-    slime.y + slime.h < food.y) {
-    } else {
-       food = {
-        x : 0,
-        y : 0,
-        w : 0,
-        h : 0,
-        color : ""
-      };
-      slimeSize();
-      setTimeout(newFood, 2500);
-    }
-}
-
-
-let slime = {
-  x : slimeX,
-  y : slimeY,
-  w : slimeWidth,
-  h : slimeHeight,
-  color : "blue"
-}
- 
-let food = {
-  x : 180,
-  y : 240,
-  w : 13,
-  h : 13,
-  color : "brown"
-};
 
 function draw(){
-  drawRect(0, 0, cvs.width, cvs.height, "green");
+  drawRect(canvas.x, canvas.y, canvas.w, canvas.h, canvas.color);
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = "black";
+  for (let x = 0; x < bw; x += 25){
+    for(let y = 0; y < bh; y += 25){
+      ctx.strokeRect(x, y, cvs.height, cvs.width);
+    }
+  }
+ 
+  
   // Food
   drawRect(food.x, food.y, food.w, food.h, food.color);
   // Rimuru
   drawRect(slime.x, slime.y, slime.w, slime.h, slime.color);
-  // New slime
-  // drawRect(newSlime.x, newSlime.y, newSlime.w, newSlime.h, newSlime.color);
-}
+ }
 
-// game movements / updates
+
+ // game movements / updates
 function update(){
   // Slime Movement
-  if(rightPressed && slime.x < cvs.width-slime.w) {
-    slime.x += 4;
+if(rightPressed && slime.x < cvs.width-slime.w) {
+    
+  
 }
 else if(leftPressed && slime.x > 0) {
-    slime.x -= 4;
+    slime.x -= 5;
 }
-else if(upPressed && slimeY < cvs.height-slime.h) {
-  slime.y -= 4;
+else if(upPressed && slime.y < cvs.height-slime.h) {
+  slime.y -= 5;
 }
 else if(downPressed && slime.y > 0) {
-  slime.y += 4;
+  slime.y += 5;
  } 
 }
 
+
+
 function game(){
-  update();
-  draw();
-  gameOver();
-  rectsColliding();
+ draw();
+ update();
+
 }
 const framesPerSecond = 50;
 setInterval(game, 1000/framesPerSecond);
-
 
